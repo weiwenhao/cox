00aac97e2ee2d9c5741731fb50f223436c67f43c
weiwenhao <1101140857@qq.com>
2020-12-12 00:26:23 +0800

    feat: string 写到一半

--- compiler.h
+++ compiler.h
@@ -2,6 +2,7 @@
 #define COX__COMPILER_H_
 
 #include "chunk.h"
+#include "object.h"
 
 bool compile(const char* source, Chunk* chunk);
 
--- memory.h
+++ memory.h
@@ -4,6 +4,9 @@
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2) // 这又是什么神奇的表达式？
 
+#define ALLOCATE(type, count) \
+  (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
 // （type*) 把申请的内存空间定义为 utf8_t 类型, 其实这一段空间已经是一块固定长度的数组咯
 #define GROW_ARRAY(previous, type, oldCount, count) \
     (type*)reallocate(previous, sizeof(type) * (oldCount), \
